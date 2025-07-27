399e6b5e2324bb734d9d34200893e38f6925f6ba
Adarsh Bajpai <adarsh.bajpai@cars24.com>
2025-07-28 01:38:39 +0530

    setting up

--- src/main/java/com/adarsh/learning/HelloController.java
+++ src/main/java/com/adarsh/learning/HelloController.java
@@ -12,6 +12,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello from Adarsh's Spring Bootss Application!";
+        return "Hello from Adarsh's Spring Boots Application!";
     }
 }
